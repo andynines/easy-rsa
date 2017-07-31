5c9c005c72372ca8270422855a92b8d9b87ff961
AardvarkNines <andrewsenin@comcast.net>
2017-07-31 15:50:16 -0400

    Update easy_rsa.py

--- easy_rsa.py
+++ easy_rsa.py
@@ -3,7 +3,7 @@
 """
 MIT License
 
-Copyright AardvarkNines (c) 2017 
+Copyright (c) 2017 AardvarkNines
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -24,6 +24,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+#setup
 from math import ceil, floor, sqrt
 from random import SystemRandom
 SR = SystemRandom()
@@ -32,9 +33,23 @@ class PublicKeyException(Exception):
     pass
 class PrivateKeyException(Exception):
     pass
+"""
+Thrown if the calculation of the respective variable fails. When using the
+Cipher class object, these errors are caught and a new set of variables is used.
+"""
+class MessageException(Exception):
+    pass
+"""
+Thrown if a given message for a new Cipher object would break the calculation or
+create an infinite loop.
+"""
 
-#prime numbers
+#prime number functions
 def is_prime(number):
+    """
+    Returns true or false for whether the given number is prime. Primality is
+    not defined mathematically for non-integers or values less than 2.
+    """
     if number < 2:
         return False
     for factor in range(2, floor(sqrt(number)) + 1):
@@ -43,57 +58,134 @@ def is_prime(number):
     return True
 
 def relatively_prime(a, b):
+    """
+    Returns true or false for whether the given numbers are relatively prime,
+    having no common factors other than 1.
+    """
     for number in range(2, min(a, b) + 1):
         if a % number == b % number == 0:
             return False
     return True
 
 def sieve(limit):
+    """
+    Returns a list of prime numbers up to but not including the given number.
+    """
     array = [True] * limit                          
     array[0] = array[1] = False
-    for (number, primality) in enumerate(array):
-        if primality:
-            yield number
+    primes = []
+    for (number, prime) in enumerate(array):
+        if prime:
+            primes.append(number)
             for index in range(number * number, limit, number):
                 array[index] = False
+    return primes
 
-random_integer = lambda minimum, maximum: SR.randint(minimum, maximum)
+def random_integer(minimum, maximum, prime=False, exclusions=[]):
+    """
+    Returns a cryptographically secure random integer in the interval [a, b).
+    If prime is set to True, the result wil be prime. The result will not be a
+    value contained within the exclusions argument.
+    """
+    if prime:
+        numbers = sieve(maximum)
+    else:
+        numbers = list(range(minimum, maximum))
+    for number in numbers:
+        if number in exclusions or number < minimum:
+            numbers.remove(number)
+    return SR.choice(numbers)
 
-def random_prime(minimum, maximum, exclusions = []):
-    primes = [prime for prime in sieve(maximum)]
-    for prime in primes:
-        if prime in exclusions:
-            primes.remove(prime)
-    return SR.choice(primes)
-
-#rsa procedure
+#rsa functions
 formula = lambda text, key, modulus: [(character ** key) % modulus for character in text]
+"""
+Returns a list of the values of a[x]^b mod c for every element of a. This is the
+format of RSA's encryption and decryption formulas m^e mod n and c^d mod n
+respectively.
+"""
 
 totient = lambda a, b: (a - 1) * (b - 1)
+"""
+Returns an evaluation of Euler's totient function for two values a and b.
+"""
 
 def generate_public(phi):
+    """
+    Generates a public key in the interval [3, phi) raising a PublicKeyException
+    if it fails.
+    """
     for attempt in range(3, phi):
         if relatively_prime(attempt, phi):
             return attempt
     raise PublicKeyException("Could not generate e for given value of phi")
 
-MAXIMUM_COEFFICIENT = 100
+MAXIMUM_COEFFICIENT = 10
 
 def generate_private(e, phi):
+    """
+    Generates a private key given the values of the public key and ϕn using
+    brute force. If the calculation reaches MAXIMUM_COEFFICIENT before
+    succeeding, a PrivateKeyException is raised. A higher MAXIMUM_COEFFICIENT
+    value may result in OverflowError, as the private key will later be used
+    as an exponent during the decryption process.
+    """
     for coefficient in range(3, MAXIMUM_COEFFICIENT + 1):
-        attempt = (phi * coefficient + 1) / e
-        if attempt == int(attempt):
-            return int(attempt)
+        attempt = phi * coefficient + 1
+        if attempt % e == 0:
+            return int(attempt / e)
     raise PrivateKeyException("Private key calculation reached maximum phi coefficient before succeeding")
 
 class Cipher(object):
     def __init__(self, m):
-        self.m = [ord(character) for character in list(str(m))]
-        largest_m = max(self.m)
-        self.p = random_prime(ceil(sqrt(largest_m)), largest_m)
-        self.q = random_prime(ceil(sqrt(largest_m)), largest_m, exclusions = [self.p])
-        self.n = self.p * self.q
-        self.phi = totient(self.p, self.q)
-        self.e = generate_public(self.phi)
-        self.d = generate_private(self.e, self.phi)
-        self.c = formula(self.m, self.e, self.n)
+        """
+        Creates an RSA cryptosystem for a given plaintext.
+        """
+        if not m:
+            raise MessageException("Message is blank or uninterpretable")
+        while True:
+            try:
+                self.m = [ord(character) for character in list(str(m))]
+                largest_m = max(self.m)
+                self.p = random_integer(ceil(sqrt(largest_m)), largest_m, prime=True)
+                self.q = random_integer(ceil(sqrt(largest_m)), largest_m, prime=True, exclusions=[self.p])
+                self.n = self.p * self.q
+                self.phi = totient(self.p, self.q)
+                self.e = generate_public(self.phi)
+                self.d = generate_private(self.e, self.phi)
+                self.c = formula(self.m, self.e, self.n)
+                break
+            except:
+                continue
+
+#demo
+def main():
+    """
+    An I/O loop that takes messages and encrypts them for the user.
+    """
+    from sys import exit
+
+    EXIT_COMMAND = "/e"
+    
+    print("Easy RSA loaded successfully")
+    print("Provide a message for encryption or type {} to exit\n".format(EXIT_COMMAND))
+    while True:
+        message = input(">")
+        if message == EXIT_COMMAND:
+            exit()
+        try:
+            encryption = Cipher(message)
+        except MessageException:
+            print("An error occurred\n")
+            continue
+        for variable in ["Unicode ordinals (m) " + ", ".join([str(ordinal) for ordinal in encryption.m]),
+                         "First prime (p) " + str(encryption.p),
+                         "Second prime (q) " + str(encryption.q),
+                         "Modulus (n) " + str(encryption.n),
+                         "Totient (phi(n)) " + str(encryption.phi),
+                         "Public key (e) " + str(encryption.e),
+                         "Private key (d) " + str(encryption.d),
+                         "Encrypted ordinals (c) " + ", ".join([str(ordinal) for ordinal in encryption.c]) + "\n"]:
+            print(variable)
+
+if __name__ == "__main__":
+    main()
